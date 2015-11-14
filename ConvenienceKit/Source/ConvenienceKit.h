42447018d82150f4126243917ba08b62e14ca28c
Julian Grosshauser <julian@juliangrosshauser.com>
2015-11-14 20:38:14 +0100

    Add Xcode project

---
+++ ConvenienceKit/Source/ConvenienceKit.h
@@ -0,0 +1,15 @@
+//
+//  ConvenienceKit.h
+//  ConvenienceKit
+//
+//  Created by Julian Grosshauser on 14/11/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ConvenienceKit.
+FOUNDATION_EXPORT double ConvenienceKitVersionNumber;
+
+//! Project version string for ConvenienceKit.
+FOUNDATION_EXPORT const unsigned char ConvenienceKitVersionString[];
